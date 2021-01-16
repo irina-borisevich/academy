d3cc2047ba0ab802dc1da7dd242acace4c67d8d1
Vadim <wadim.sidoryk@mail.ru>
2021-01-16 20:03:31 +0300

    first commit

---
+++ academy/src/by/academy/lesson1/HelloWorld.java
@@ -0,0 +1,10 @@
+package by.academy.lesson1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello academy students!");
+	}
+
+}
