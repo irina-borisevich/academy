414bf16162311d91274622f06d3a5db91c661703
Vadim <wadim.sidoryk@mail.ru>
2021-01-16 21:28:08 +0300

    Lesson 2 commit

--- academy/src/by/academy/lesson1/HelloWorld.java
+++ academy/src/by/academy/lesson1/HelloWorld.java
@@ -3,7 +3,6 @@ package by.academy.lesson1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
         System.out.println("Hello academy students!");
 	}
 
