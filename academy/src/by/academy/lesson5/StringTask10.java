eef6468bf4df29e3d0e8e074005cb3ef4c8495d6
Vadim <wadim.sidoryk@mail.ru>
2021-01-27 21:34:21 +0300

    first commit

---
+++ academy/src/by/academy/lesson5/StringTask10.java
@@ -0,0 +1,17 @@
+package by.academy.lesson5;
+//	10. »спользу€ оператор switch, написать программу, 
+//  котора€ выводит на экран сообщени€ о принадлежности некоторого значени€ k
+//	интервалам (-10k, 0], (0, 5], (5, 10], (10, 10k].
+
+import java.util.Scanner;
+
+public class StringTask10 {
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in); 
+        String k = scn.next();
+		
+		System.out.println("Min: ");
+	    scn.close();
+	}
+
+}
