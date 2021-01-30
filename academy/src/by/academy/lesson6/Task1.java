a19a1f43916c01a361825443c7f87163d741e539
Vadim <wadim.sidoryk@mail.ru>
2021-01-30 15:41:44 +0300

    qwert

--- academy/src/by/academy/lesson6/Task1.java
+++ academy/src/by/academy/lesson6/Task1.java
@@ -1,10 +1,9 @@
 package by.academy.lesson6;
 
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class Task1 {
-	
+
 //	1. Найти в строке не только запятые, но и другие знаки препинания. Подсчитать
 //	общее их количество
 
@@ -13,17 +12,17 @@ public class Task1 {
 				+ "учесть, что слова могут разделяться несколькими пробелами, в начале и конце\r\n"
 				+ "текста также могут быть пробелы, но могут и отсутствовать?";
 		String regex = "{\\.,!&}";
-		
+
 		java.util.regex.Pattern p = java.util.regex.Pattern.compile(regex);
 		Matcher m = p.matcher(text);
-		
+
 		int counter = 0;
-		while (m.find()) { 
+		while (m.find()) {
 			counter++;
-			System.out.print(text.substring(m.start(), m.end()));		
+			System.out.print(text.substring(m.start(), m.end()));
 		}
-     	System.out.println();
-     	System.out.println(counter);
+		System.out.println();
+		System.out.println(counter);
 	}
 
 }
