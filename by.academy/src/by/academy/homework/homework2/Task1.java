dce11ca67c9016f5e41039f588eeece0df289b36
денис <@192.168.100.17>
2021-02-01 08:19:34 +0300

    commit first

--- by.academy/src/by/academy/homework/homework2/Task1.java
+++ by.academy/src/by/academy/homework/homework2/Task1.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Task1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {  
 		Scanner scn = new Scanner(System.in);
 		Boolean answer = Boolean.FALSE;
 
