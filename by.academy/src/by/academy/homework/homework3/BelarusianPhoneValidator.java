68102330ec8422354352f1973a374ee199edc556
prokat <prokat@DESKTOP-GJ8RKVQ>
2021-02-20 05:10:01 +0300

    first commit

--- by.academy/src/by/academy/homework/homework3/BelarusianPhoneValidator.java
+++ by.academy/src/by/academy/homework/homework3/BelarusianPhoneValidator.java
@@ -3,7 +3,7 @@ package by.academy.homework.homework3;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class BelarusianPhoneValidator implements Validator {
+public class BelarusianPhoneValidator implements Validator { 
 
 	private Pattern p = Pattern.compile("\\+375((29)|(33)|(44)|(25))[0-9]{7}");
 
