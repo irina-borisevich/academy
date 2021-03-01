9d68e4817692eddeb7afa347e3ccf5904b01e29a
prokat <prokat@DESKTOP-GJ8RKVQ>
2021-03-01 23:37:32 +0300

    first commit

---
+++ by.academy/src/by/academy/homework/homework4/task3/Main.java
@@ -0,0 +1,15 @@
+package by.academy.homework.homework4.task3;
+
+import java.util.Iterator;
+
+public class Main {
+	public static void main(String[] args) {
+		Integer[][] matrix = { { 1, 10, 5 }, {}, { 8, 2 }, { 7 } };
+
+		Iterator<Integer> iterator = new MatrixIterator<>(matrix);
+
+		while (iterator.hasNext()) {
+			System.out.println(iterator.next());
+		}
+	}
+}
---
+++ by.academy/src/by/academy/homework/homework4/task3/MatrixIterator.java
@@ -0,0 +1,46 @@
+package by.academy.homework.homework4.task3;
+
+import java.util.Iterator;
+
+public class MatrixIterator<T> implements Iterator<T> {
+	private int size;
+	private int position = 0;
+	private int row = 0;
+	private int col = 0;
+	private T[][] matrix;
+
+	public MatrixIterator(T[][] matrix) {
+		this.matrix = matrix;
+		this.size = countElements(matrix);
+	}
+
+	private int countElements(T[][] matrix) {
+		int count = 0;
+		for (T[] row : matrix) {
+			count += row.length;
+		}
+		System.out.println("The number of elements in the matrix: " + count);
+		return count;
+	}
+
+	@Override
+	public boolean hasNext() {
+		return position < size;
+	}
+
+	@Override
+	public T next() {
+		if (position < size) {
+			T element = matrix[row][col];
+			position++;
+			col++;
+			while (row < matrix.length && col >= matrix[row].length) { // пропускаем пустые строки
+				col = 0;
+				row++;
+			}
+			return element;
+		} else {
+			return null;
+		}
+	}
+}
