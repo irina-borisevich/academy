88b0a47fac006e283f1aaf389e103b05626310b3
prokat <prokat@DESKTOP-GJ8RKVQ>
2021-03-04 20:23:24 +0300

    first commit

---
+++ by.academy/src/by/academy/homework/homework4/task3/Main.java
@@ -0,0 +1,14 @@
+package by.academy.homework.homework4.task3;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Integer[][] matrix = {{1, 2, 3}, {}, {4, 5}, {6}};
+		MatrixIterator<Integer> iterator = new MatrixIterator<>(matrix);
+		
+		while (iterator.hasNext()) {
+		    System.out.println(iterator.next());
+		}
+	}
+
+}
