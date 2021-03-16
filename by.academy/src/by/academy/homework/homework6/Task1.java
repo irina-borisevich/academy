900bf5b89e99e7791dbd7f5562f375952d9ca380
денис <@192.168.100.17>
2021-03-16 20:25:01 +0300

    first commit

---
+++ by.academy/src/by/academy/homework/homework6/Task1.java
@@ -0,0 +1,39 @@
+package by.academy.homework.homework6;
+
+import java.util.Scanner;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		String end = "stop";
+		Scanner scn = new Scanner(System.in);
+
+		File dir = new File("C:/IVB");
+
+		if (!dir.exists()) {
+			dir.mkdirs();
+		}
+
+		File file = new File(dir, "task1.txt");
+
+		System.out.println("Enter information (end - string \"stop\"):");
+
+		String str = scn.nextLine();
+		try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file))) {
+			while (!str.equals(end)) {
+				bufferedWriter.write(str);
+				bufferedWriter.newLine();
+				str = scn.nextLine();
+			}
+			System.out.println("Information written to file " + file.getAbsolutePath() + "!");
+		} catch (IOException ex) {
+			System.out.println(ex.getMessage());
+		}
+
+		scn.close();
+	}
+}
