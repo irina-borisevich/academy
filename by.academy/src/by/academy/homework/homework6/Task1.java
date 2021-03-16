a6e79b138166f822aca4ddd996cf62584d4b8640
денис <@192.168.100.17>
2021-03-16 20:30:50 +0300

    first commit

--- by.academy/src/by/academy/homework/homework6/Task1.java
+++ by.academy/src/by/academy/homework/homework6/Task1.java
@@ -8,7 +8,7 @@ import java.io.IOException;
 
 public class Task1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		String end = "stop";
 		Scanner scn = new Scanner(System.in);
 
