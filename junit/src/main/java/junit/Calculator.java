4944e279e294faf0e7b20c2153d303d1400293af
денис <@192.168.100.17>
2021-03-24 00:59:01 +0300

    first commit

---
+++ junit/src/main/java/junit/Calculator.java
@@ -0,0 +1,16 @@
+package junit;
+
+public class Calculator {
+
+	public static int getSum(int x, int y) {
+		return x + y;
+	}
+
+	public static int getSubtract(int x, int y) {
+		return x - y;
+	}
+
+	public static int getMultiple(int x, int y) {
+		return x * y;
+	}
+}
